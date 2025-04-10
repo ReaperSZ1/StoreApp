372f104500ae89ca89832506429e4ccc914a3efe
gabriel <gabrielsil20177@gmail.com>
2025-04-10 20:04:03 -0300

    card de promotions adicionado e requisicao de dados dos produtos feitos com dato

--- src/Controllers/HomeController.js
+++ src/Controllers/HomeController.js
@@ -1,12 +1,15 @@
+import { buscarProdutos } from '../utils/dato.js';
+
 class HomeController {
-    static async index(req, res) {
-        try {
-            res.render('index');
-        } catch (error) {
-            console.error('Erro ao carregar a página inicial:', error);
-            res.status(500).json({ error: 'Erro interno do servidor' });
-        }
-    }
+	static async index(req, res) {
+		try {
+			const products = await buscarProdutos();
+			res.render('index', { products });
+		} catch (error) {
+			console.error('Erro ao carregar a página inicial:', error);
+			res.status(500).json({ error: 'Erro interno do servidor' });
+		}
+	}
 }
 
 export default HomeController;
---
+++ src/utils/dato.js
@@ -0,0 +1,49 @@
+// utils/dato.js
+const API_URL = "https://graphql.datocms.com/";
+const API_TOKEN = "3fe2bbeef4b29e5444a911555d3ca8";
+
+const query = `
+  query {
+    allProducts {
+        id
+        title
+        slug
+        description
+        price
+        onsale
+        category {
+            title
+        }
+        img {
+            url
+        }
+    }
+  }
+`;
+
+
+export async function buscarProdutos() {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ query })
+    });
+
+    const result = await response.json();
+
+    if (result.errors) {
+      console.error("❌ Erros ao buscar produtos:", result.errors);
+      return [];
+    } else {
+      return result.data.allProducts;
+    }
+  } catch (error) {
+    console.error("❌ Erro de rede:", error.message);
+    return [];
+  }
+}
