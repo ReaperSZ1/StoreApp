07029699ab87a7203b087a6c0f52f7b91fe01cf4
gabriel <gabrielsil20177@gmail.com>
2025-04-23 17:03:47 -0300

    git add

---
+++ src/Controllers/facebook.controller.js
@@ -0,0 +1,47 @@
+import passport from 'passport';
+
+export const facebookSignup = passport.authenticate('facebook-signup', {
+    scope: ['email']
+});
+
+export const facebookSignupCallback = (req, res, next) => {
+    passport.authenticate('facebook-signup', (err, user, info) => {
+        if (err) { return next(err); }
+
+        if (!user) {
+            req.flash('errorMsg', info.message);
+            return res.redirect('/');
+        }
+
+        req.login(user, (err) => {
+            if (err) { return next(err); }
+            req.session.user = user.id;
+            req.session.isLoggedIn = true;
+            req.flash('successMsg', 'Signed up successfully!');
+            return res.redirect('/');
+        });
+    })(req, res, next);
+};
+
+export const facebookLogin = passport.authenticate('facebook-login', {
+    scope: ['email']
+});
+
+export const facebookLoginCallback = (req, res, next) => {
+    passport.authenticate('facebook-login', (err, user, info) => {
+        if (err) { return next(err); }
+
+        if (!user) {
+            req.flash('errorMsg', info.message);
+            return res.redirect('/');
+        }
+
+        req.login(user, (err) => {
+            if (err) { return next(err); }
+            req.session.user = user.id;
+            req.session.isLoggedIn = true;
+            req.flash('successMsg', 'login successfully!');
+            return res.redirect('/');
+        });
+    })(req, res, next);
+};
