9d926b8e78532d3511868e57bce1be23ac891377
gabriel <gabrielsil20177@gmail.com>
2025-05-16 15:55:17 -0300

    ✨feat: Setup e2e test pipeline and adjustments for proper workflow execution.

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    provider VARCHAR(255) DEFAULT 'local',
+    googleId VARCHAR(255),
+    facebookId VARCHAR(255),
+    favorites JSON DEFAULT (JSON_ARRAY()),
+    cart JSON DEFAULT (JSON_ARRAY())
+);
